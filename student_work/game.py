b694e7b2c40f4ea94bc75cc743b788c4f2c5d58e
Eli-Ba <s1006974@student.knoxschools.org>
2026-02-26 16:05:04 +0000

    updates

--- student_work/game.py
+++ student_work/game.py
@@ -1,11 +1,13 @@
-# Write your game here
+# # Write your game here
 
-game_data = {
-    # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
-}
+# game_data = {
+#     # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
+# }
 
-def draw_board(screen):
-    # Print the board and all game elements using curses
+# def draw_board(screen):
+#     # Print the board and all game elements using curses
 
 
-# Good Luck!
+# # Good Luck!
+
+# print(:green_square:)
