8be6ba3f35df959d564346750e6b85ddc537fe06
Eli-Ba <s1006974@student.knoxschools.org>
2026-03-05 16:48:02 +0000

    fixes for betterness

--- student_work/game.py
+++ student_work/game.py
@@ -9,6 +9,7 @@
 # To make this work, you may have to type this into the terminal --> pip install curses
 import curses
 import time
+import random
 
 game_data = {
     'width': 10,
@@ -19,60 +20,70 @@ game_data = {
 
     # ASCII icons
     'ball': "\U000026AA",
-    'block': "\U0001F532\U0001F532\U0001F532\U0001F532\U0001F532\U0001F532\U0001F532\U0001F532\U0001F532",
+    'block': "\U0001F532",
     'paddle': "\U00002796\U00002796",
     'empty': "  "
 }
 
-
 def draw_board(stdscr):
+    curses.start_color()
+    curses.use_default_colors()
+    curses.init_pair(1, curses.COLOR_WHITE, -1)
+
     stdscr.clear()
     for y in range(game_data['height']):
         row = ""
         for x in range(game_data['width']):
+            # Player
             if x == game_data['player']['x'] and y == game_data['player']['y']:
                 row += game_data['paddle']
+            # Block
             elif x == game_data['block_pos']['x'] and y == game_data['block_pos']['y']:
                 row += game_data['block']
+            # Ball
             elif x == game_data['ball_pos']['x'] and y == game_data['ball_pos']['y']:
                 row += game_data['ball']
             else:
                 row += game_data['empty']
-        stdscr.addstr(y, 0, row)
-    stdscr.refresh()
+        stdscr.addstr(y, 0, row, curses.color_pair(1))
 
 def move_player(key):
     x = game_data['player']['x']
-    if key.lower() == "a" and x > 0:
-        game_data['player']['x'] -= 1
-    elif key.lower() == "d" and x < game_data['width'] - 1:
-        game_data['player']['x'] += 1
+    y = game_data['player']['y']
+
+    new_x, new_y = x, y
+    key = key.lower()
+
+    if key == "a" and x > 0:
+        new_x -= 1
+    elif key == "d" and x < game_data['width'] - 1:
+        new_x += 1
+    else:
+        return  # Invalid key or move off board
+
+    # Update position and increment score
+    game_data['player']['x'] = new_x
+    game_data['player']['y'] = new_y
+
 
 def move_ball():
-    ball = game_data['ball_pos']
-    
-    # Calculate next position
-    new_x = ball['x'] + ball['dx']
-    new_y = ball['y'] + ball['dy']
+    directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+    random.shuffle(directions)
+    ex, ey = game_data['ball_pos']['x'], game_data['ball_pos']['y']
 
-    # Wall Collision Logic
-    # Bounce Left/Right
-    if new_x < 0 or new_x >= game_data['width']:
-        ball['dx'] *= -1
-        new_x = ball['x'] + ball['dx']
-
-    # Bounce Top/Bottom
-    if new_y < 0 or new_y >= game_data['height']:
-        ball['dy'] *= -1
-        new_y = ball['y'] + ball['dy']
-
-    # Update actual position
-    ball['x'] = new_x
-    ball['y'] = new_y
+    for dx, dy in directions:
+        new_x = ex + dx
+        new_y = ey + dy
+        if 0 <= new_x < game_data['width'] and 0 <= new_y < game_data['height']:
+                game_data['ball_pos']['x'] = new_x
+                game_data['ball_pos']['y'] = new_y
+                break
 
 def main(stdscr):
     curses.curs_set(0)
     stdscr.nodelay(True)
+
+    draw_board(stdscr)
 
     while True:
         try:
@@ -80,14 +91,14 @@ def main(stdscr):
         except:
             key = None
 
-        if key and key.lower() == "q":
-            break
-        
         if key:
+            if key.lower() == "q":
+                break
             move_player(key)
 
-        move_ball()
-        draw_board(stdscr)
-        time.sleep(0.1)
+            move_ball()
 
-    curses.wrapper(main)
+            draw_board(stdscr)
+            time.sleep(0.2)
+
+curses.wrapper(main)   
