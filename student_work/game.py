450971b80f13522be6ae5d2358e90078381ad534
Eli-Ba <s1006974@student.knoxschools.org>
2026-03-05 16:27:12 +0000

    undoing mr jacksons doing

--- student_work/game.py
+++ student_work/game.py
@@ -21,7 +21,7 @@ game_data = {
     # ASCII icons
     'ball': "\U000026AA",
     'block': "\U0001F532",
-    'paddle': "\U00002796,\U00002796",
+    'paddle': "\U00002796\U00002796",
     'empty': "  "
 }
 
